cf5898d9a2a98292230fc50b67aad69995f20c91
Markus Knapp <github@direcs.de>
2024-07-28 20:36:25 +0200

    moved file

--- VL530X_multi_sensor/VL530X_multi_sensor.ino
+++ VL530X_multi_sensor/VL530X_multi_sensor.ino
@@ -1,5 +1,19 @@
+#include <Adafruit_VL53L0X.h>
+#include <vl53l0x_api.h>
+#include <vl53l0x_api_calibration.h>
+#include <vl53l0x_api_core.h>
+#include <vl53l0x_api_ranging.h>
+#include <vl53l0x_api_strings.h>
+#include <vl53l0x_def.h>
+#include <vl53l0x_device.h>
+#include <vl53l0x_i2c_platform.h>
+#include <vl53l0x_interrupt_threshold_settings.h>
+#include <vl53l0x_platform.h>
+#include <vl53l0x_platform_log.h>
+#include <vl53l0x_tuning.h>
+#include <vl53l0x_types.h>
+
 #include <Wire.h>
-#include <VL53L0X.h>
 
 /*
 VL53L0X sensor7;
