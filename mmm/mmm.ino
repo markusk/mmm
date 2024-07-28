6bb776560e79b3685201f3dda11242d60980e2c7
Markus Knapp <github@direcs.de>
2024-07-28 20:53:41 +0200

    added MIDI USB methods

--- mmm/mmm.ino
+++ mmm/mmm.ino
@@ -1,6 +1,35 @@
 #include "Adafruit_VL53L0X.h"
+#include "MIDIUSB.h"
 
 Adafruit_VL53L0X lox = Adafruit_VL53L0X();
+
+// First parameter is the event type (0x09 = note on, 0x08 = note off).
+// Second parameter is note-on/note-off, combined with the channel.
+// Channel can be anything between 0-15. Typically reported to the user as 1-16.
+// Third parameter is the note number (48 = middle C).
+// Fourth parameter is the velocity (64 = normal, 127 = fastest).
+void noteOn(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity)
+{
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+// First parameter is the event type (0x0B = control change).
+// Second parameter is the event type, combined with the channel.
+// Third parameter is the control number number (0-119).
+// Fourth parameter is the control value (0-127).
+void controlChange(byte channel, byte control, byte value)
+{
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
+
 
 void setup() {
   Serial.begin(115200);
