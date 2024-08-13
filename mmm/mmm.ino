b4bee27e4a5f7e7e570fcfac58facbd973031160
Markus Knapp <github@direcs.de>
2024-08-13 10:53:02 +0200

    added LED blinking when turning MIDI note on/off

--- mmm/mmm.ino
+++ mmm/mmm.ino
@@ -64,6 +64,9 @@ void noteOn(byte channel, byte pitch, byte velocity)
 {
   midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOn);
+
+  // fancy LED blinking...
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 
@@ -71,6 +74,9 @@ void noteOff(byte channel, byte pitch, byte velocity)
 {
   midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
   MidiUSB.sendMIDI(noteOff);
+
+  // fancy LED blinking...
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 
